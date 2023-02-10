ba7dd89508bca74b8cf127f889030e8f88b83a8c
HollywoodOKC <T_Lane11@yahoo.com>
2023-02-09 20:50:41 -0600

    Updated.

---
+++ orders.sql
@@ -0,0 +1,22 @@
+CREATE TABLE orders (
+  id SERIAL PRIMARY KEY,
+  order_id INTEGER,
+  person_id INTEGER,
+  product_name TEXT,
+  product_price INTEGER
+)
+
+INSERT INTO orders (order_id, person_id, product_name, product_price)
+VALUES (78334, 2115, 'iMac', 2000);
+VALUES (56219, 2971, 'MacBook Pro', 3500);
+VALUES (13921, 8253, 'iPad', 1000);
+VALUES (99123, 5724, 'iPhone', 1000);
+VALUES (25786, 4423, 'MacBook Air', 1500);
+
+SELECT * FROM orders;
+SELECT SUM(product_name) FROM orders;
+SELECT SUM(product_price) FROM orders;
+
+SELECT order_id, order_id = 78334
+FROM orders
+GROUP BY order_id;
--- person.sql
+++ person.sql
@@ -1,6 +1,63 @@
-Table person (
-  id SERIAL Primary Key,
-  name TEXT,
+CREATE TABLE person (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(40),
   age NUMBER,
   height NUMBER
-)
+  city VARCHAR(40),
+  favorite_color VARCHAR(40)
+);
+
+INSERT INTO person (name, age, height, city, favorite_color)
+VALUES ('Johnny', 30, 190.5, 'Chicago', 'Red')
+VALUES ('Alexandra', 29, 165, 'Tulsa', 'Orange')
+VALUES ('Holly', 47, 172.7, 'Oklahoma City', 'Black')
+VALUES ('Sam', 40, 182.2, 'Flagstaff', 'Gold')
+VALUES ('Lance', 18, 180.3, 'Portland', 'Green')
+
+SELECT height, MAX(price)
+FROM person
+GROUP BY height;
+
+SELECT height, MIN(price)
+FROM person
+GROUP BY height;
+
+SELECT age, MAX(price)
+FROM person
+GROUP BY age;
+
+SELECT age, age > 20
+FROM person
+GROUP BY age;
+
+SELECT age, age = 18
+FROM person
+GROUP BY age;
+
+SELECT age, IN('age < 20', 'age > 30')
+FROM person
+GROUP BY age;
+
+SELECT age, age != 27
+FROM person
+GROUP BY age;
+
+SELECT favorite_color, favorite_color != 'Red'
+FROM person
+GROUP BY favorite_color;
+
+SELECT favorite_color, IN(favorite_color != 'Red', favorite_color != 'Blue')
+FROM person
+GROUP BY favorite_color;
+
+SELECT favorite_color, IN(favorite_color = 'Orange', favorite_color = 'Green')
+FROM person
+GROUP BY favorite_color;
+
+SELECT favorite_color, IN(favorite_color = 'Orange', favorite_color = 'Green', favorite_color = 'Blue')
+FROM person
+GROUP BY favorite_color;
+
+SELECT favorite_color, IN(favorite_color = 'Yellow', favorite_color = 'Purple')
+FROM person
+GROUP BY favorite_color;
