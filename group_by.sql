594e3a25cecda1d00eb546bb64cc36ba5d26d5c5
HollywoodOKC <T_Lane11@yahoo.com>
2023-04-19 23:52:45 -0500

    Added to group_by

---
+++ group_by.sql
@@ -0,0 +1,3 @@
+SELECT SUM(total), billing_state FROM invoice GROUP BY billing_state;
+SELECT AVG(milliseconds) FROM track GROUP BY album_id ORDER BY 1;
+SELECT COUNT(*) FROM album WHERE artist_id IN (8, 22) GROUP BY artist_id;
